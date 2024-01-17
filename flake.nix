d15d3ed3e22c8e167cd1412cda4a374f9b438e3c
Matthew Toohey <contact@mtoohey.com>
2024-01-17 00:28:43 -0500

    initial, literal, invalid translation

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "tree-sitter-ld";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }: {
+    overlays.default = _: prev: {
+      tree-sitter = prev.tree-sitter.override {
+        extraGrammars.tree-sitter-ld = {
+          language = "ld";
+          inherit (prev.tree-sitter) version;
+          src = _: builtins.path { path = ./.; name = "ld-src"; };
+        };
+      };
+    };
+  } // utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        overlays = [ self.overlays.default ];
+        inherit system;
+      };
+      inherit (pkgs) mkShell nodejs nodePackages python3 tree-sitter typescript;
+    in
+    {
+      packages.default = tree-sitter.builtGrammars.tree-sitter-ld;
+
+      devShells.default = mkShell {
+        packages = [
+          nodejs
+          nodePackages.typescript-language-server
+          python3
+          tree-sitter
+          typescript
+        ];
+      };
+    });
+}
